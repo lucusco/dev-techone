4dea28a67f08135acf65b214a3b1cb9adb299bac
Luis Bueno <lu.cusco@gmail.com>
2021-04-09 22:23:03 -0300

    Refactoring classe DataRecord

--- Lib/Api/DataRecord.php
+++ Lib/Api/DataRecord.php
@@ -5,7 +5,6 @@ namespace Techone\Lib\Api;
 use PDO;
 use PDOException;
 use Techone\Lib\Database\Connection;
-use Techone\Lib\Model\Ramal;
 
 /**
  *  Layer Supertype Pattern - baseada em Active Record\
@@ -16,9 +15,10 @@ abstract class DataRecord
 {
 
     abstract function setId($id);
+    abstract function getId();
 
     /**
-     *  Obtém o nome da entidade sendo manipulada
+     *  Obtém o nome da tabela do banco de dados a ser manipulada
      */
     private function getEntity()
     {
@@ -31,40 +31,40 @@ abstract class DataRecord
      *  @param $object  Array com os dados do objeto
      *  @return object|null Retorna o objeto com o id setado ou null em caso de falha
      */
-    protected function store(array $object): ?object
+    protected function store(array $object): bool
     {
         $object = self::prepare($object);
 
-        if (!isset($object['id'])) { // INSERT    
-            $object['id'] = $id = $this->getProximoId();
+        if ($this->loadFromId() === false) { // INSERT    
             $columns = implode(', ', array_keys($object));
+            foreach ($object as $column => $value) {
+                $valores[] = ":$column";
+            }
 
-            $query = "INSERT INTO {$this->getEntity()} ({$columns}) VALUES (" . rtrim(str_repeat('?,', count($object)), ','). ")"; 
+            $query = "INSERT INTO {$this->getEntity()} ({$columns}) VALUES (" . implode(', ', $valores). ")"; 
 
         } else { //UPDATE
             $id = array_shift($object);
             foreach ($object as $column => $value) {
-                $sets[] = "$column = ?";
+                $sets[] = "$column = :$column";
             }
 
             $query = "UPDATE {$this->getEntity()} SET " . implode(', ', $sets) . " WHERE id ={$id}";
         }
 
-        $conn = Connection::getConnection();
-        $stmt = $conn->prepare($query);
-        foreach (array_values($object) as $key => $value) {
-            $tipo = is_numeric($value) ? PDO::PARAM_INT : PDO::PARAM_STR;
-            $stmt->bindValue(($key+1), $value, $tipo);
-        }
-        $result = $stmt->execute();
+        try {
+            $conn = Connection::getConnection();
+            $stmt = $conn->prepare($query);
+            foreach ($object as $column => $value) {
+                $tipo = is_numeric($value) ? PDO::PARAM_INT : PDO::PARAM_STR;
+                $stmt->bindValue(":$column", $value, $tipo);
+            }
+            return $stmt->execute();
 
-        if ($result) {
-            $this->setId($id);
-            return $this;
-        } else {
-            return null;
+
+        } catch (PDOException $e) {
+            return $e->getMessage();
         }
-        
     }
 
     /**
@@ -78,20 +78,28 @@ abstract class DataRecord
     protected function load(int $id = null, string $coluna = '*', string $order = 'id')
     {
         $sql = isset($id) ? "SELECT $coluna FROM {$this->getEntity()} WHERE id = $id" : "SELECT $coluna FROM {$this->getEntity()} ORDER BY $order";
-        
-        if ($conn = Connection::getConnection()) {
+        try {
+            $conn = Connection::getConnection();
             $stmt = $conn->query($sql);
             $tipoFetch = is_numeric($id) ? 'fetch' : 'fetchAll';
             return $stmt->$tipoFetch(PDO::FETCH_OBJ);
-        }   
+
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+         
     }
 
-    protected function loadFromId($id)
+    protected function loadFromId()
     {
-        $conn = Connection::getConnection();
-        $sql = "SELECT * FROM {$this->getEntity()} WHERE id = $id";
-        return $conn->query($sql)->fetchObject(get_class($this));
-        
+        try {
+            $conn = Connection::getConnection();
+            $sql = "SELECT * FROM {$this->getEntity()} WHERE id = {$this->getId()}";
+            return $conn->query($sql)->fetchObject(get_class($this));
+
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
         /* Forma alternativa */
         //$stmt = $conn->query($sql);
         //$stmt->setFetchMode(PDO::FETCH_CLASS, get_class($this));
@@ -107,9 +115,9 @@ abstract class DataRecord
     {
         try {
             $conn = Connection::getConnection();
-            $stmt = $conn->query("SELECT COALESCE(max(id), 0) AS ultimo FROM {$this->getEntity()}");
-            $result = $stmt->fetch();
-            return $result['ultimo'] + 1;
+            $stmt = $conn->query("SELECT nextval('" . $this->getEntity() . "_id_seq') AS proximo");
+            return $stmt->fetch()['proximo'];
+
         } catch (PDOException $e) {
             return $e->getMessage();
         }
@@ -146,22 +154,19 @@ abstract class DataRecord
      *
      * @param string $coluna Coluna do BD a ser vasculhada
      * @param mixed $valor Valor procurado
-     * @param int $id ID do objeto
      * @return true|false 
      */
-    protected function jaExiste($coluna, $valor, $id)
-    {        
-        $sql = "SELECT id FROM {$this->getEntity()} WHERE $coluna = $valor";
-        $conn = Connection::getConnection();
-        $stmt = $conn->query($sql);
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    protected function jaExiste($coluna, $valor): bool
+    {   
+        try{
+            $sql = "SELECT id FROM {$this->getEntity()} WHERE $coluna = '$valor' AND id != {$this->getId()}";
+            $conn = Connection::getConnection();
+            $stmt = $conn->query($sql);
 
-        // Validações com base no id retornado
-        if (!$result) $ret = false;
-        else if (count($result) > 1) $ret = true;
-        else if ($result[0]['id'] != $id) $ret = true;
-        else $ret = false;
+            return  $stmt->fetch(PDO::FETCH_ASSOC) ? true : false;
 
-        return $ret;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }     
     }
 }
